1207c644ce273100047984f276da7d8bdb80c596
Stephen A <StephenAdams.Minn@gmail.com>
2013-08-25 10:51:18 -0500

    Added test for uneven binding vector

--- test/errors/core_test.clj
+++ test/errors/core_test.clj
@@ -9,6 +9,11 @@
 (def class-cast-exception (java.lang.ClassCastException. "oneType cannot be cast to anotherType"))
 (def bigint-illegal-arg-ex (java.lang.IllegalArgumentException. "contains? not supported on type: clojure.lang.BigInt"))
 (def best-approximation (ns-resolve 'errors.dictionaries 'best-approximation))
+(def incorrect-let-exception (try (eval (read-string "(let [l 1 s] (println l))")) (catch Exception e e)))
+
+(defn get-pretty-message-string [e]
+   (-> e get-pretty-message get-all-text)
+   )
 
 (deftest test-best-approximation
    (is (= "unrecognized type oneType" (best-approximation "oneType")))
@@ -17,7 +22,8 @@
    )
 
 (deftest test-get-pretty-message
-   (is (= "Test Message" (get-all-text (get-pretty-message simple-non-match-exception))))
-   (is (= "Attempted to use unrecognized type oneType, but unrecognized type anotherType was expected." (get-all-text (get-pretty-message class-cast-exception))))
-   (is (= "Function contains? does not allow a number as an argument" (get-all-text (get-pretty-message bigint-illegal-arg-ex))))
+   (is (= "Test Message" (get-pretty-message-string simple-non-match-exception)))
+   (is (= "Attempted to use unrecognized type oneType, but unrecognized type anotherType was expected." (get-pretty-message-string class-cast-exception)))
+   (is (= "Function contains? does not allow a number as an argument" (get-pretty-message-string bigint-illegal-arg-ex)))
+   (is (= "" (get-pretty-message-string incorrect-let-exception)))
    )
